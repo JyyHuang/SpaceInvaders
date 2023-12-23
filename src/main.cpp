3e14550066931d2f178f18f3a4f0b56e9330181b
Joey Huang <joeyh6547@gmail.com>
2023-12-22 19:17:57 -0500

    display screen and sprite

---
+++ src/main.cpp
@@ -0,0 +1,262 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <cstdint>
+
+using namespace std; 
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void validate_shader(GLuint shader, const char *file = 0){
+    static const unsigned int BUFFER_SIZE = 512;
+    char buffer[BUFFER_SIZE];
+    GLsizei length = 0;
+
+    glGetShaderInfoLog(shader, BUFFER_SIZE, &length, buffer);
+
+    if(length>0){
+        cout << "Shader " << shader << "compiler error: " << buffer << endl;
+    }
+}
+
+bool validate_program(GLuint program){
+    static const GLsizei BUFFER_SIZE = 512;
+    GLchar buffer[BUFFER_SIZE];
+    GLsizei length = 0;
+
+    glGetProgramInfoLog(program, BUFFER_SIZE, &length, buffer);
+
+    if(length>0){
+        cout << "Program " << program << "link error: " << buffer;
+        return false;
+    }
+
+    return true;
+}
+
+struct Buffer
+{
+    size_t width, height;
+    uint32_t* data;
+};
+
+struct Sprite
+{
+    size_t width, height;
+    uint8_t* data;
+};
+
+void buffer_clear(Buffer* buffer, uint32_t color)
+{
+    for(size_t i = 0; i < buffer->width * buffer->height; ++i)
+    {
+        buffer->data[i] = color;
+    }
+}
+
+// Goes over sprite and draws pixels at coordinates (x,y)
+void buffer_draw_sprite(Buffer* buffer, const Sprite& sprite, size_t x, size_t y, uint32_t color)
+{
+    for(size_t xi = 0; xi < sprite.width; ++xi)
+    {
+        for(size_t yi = 0; yi < sprite.height; ++yi)
+        {
+            if(sprite.data[yi * sprite.width + xi] &&
+               (sprite.height - 1 + y - yi) < buffer->height &&
+               (x + xi) < buffer->width)
+            {
+                buffer->data[(sprite.height - 1 + y - yi) * buffer->width + (x + xi)] = color;
+            }
+        }
+    }
+}
+
+uint32_t rgba_to_uint32(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+{
+    return (r << 24) | (g << 16) | (b << 8) | a;
+}
+
+void create_shaders()
+{
+    // Create shaders
+    static const char* fragment_shader =
+        "\n"
+        "#version 330\n"
+        "\n"
+        "uniform sampler2D buffer;\n"
+        "noperspective in vec2 TexCoord;\n"
+        "\n"
+        "out vec3 outColor;\n"
+        "\n"
+        "void main(void){\n"
+        "    outColor = texture(buffer, TexCoord).rgb;\n"
+        "}\n";
+
+    static const char* vertex_shader =
+        "\n"
+        "#version 330\n"
+        "\n"
+        "noperspective out vec2 TexCoord;\n"
+        "\n"
+        "void main(void){\n"
+        "\n"
+        "    TexCoord.x = (gl_VertexID == 2)? 2.0: 0.0;\n"
+        "    TexCoord.y = (gl_VertexID == 1)? 2.0: 0.0;\n"
+        "    \n"
+        "    gl_Position = vec4(2.0 * TexCoord - 1.0, 0.0, 1.0);\n"
+        "}\n";
+
+    GLuint shader_id = glCreateProgram();
+
+    {
+        //Create vertex shader
+        GLuint vshader = glCreateShader(GL_VERTEX_SHADER);
+
+        glShaderSource(vshader, 1, &vertex_shader, 0);
+        glCompileShader(vshader);
+        validate_shader(vshader, vertex_shader);
+        glAttachShader(shader_id, vshader);
+
+        glDeleteShader(vshader);
+    }
+
+    {
+        //Create fragment shader
+        GLuint fshader = glCreateShader(GL_FRAGMENT_SHADER);
+
+        glShaderSource(fshader, 1, &fragment_shader, 0);
+        glCompileShader(fshader);
+        validate_shader(fshader, fragment_shader);
+        glAttachShader(shader_id, fshader);
+
+        glDeleteShader(fshader);
+    }
+
+    glLinkProgram(shader_id);
+
+    glUseProgram(shader_id);
+
+    GLint location = glGetUniformLocation(shader_id, "buffer");
+    glUniform1i(location, 0);
+}
+
+int main(int argc, char* argv[])
+{
+    const size_t buffer_width = 225;
+    const size_t buffer_height = 225;
+
+    if (!glfwInit())
+    {
+        cout << "Failed to initialize GLFW" << endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(buffer_width, buffer_height, "Space Invaders", NULL, NULL);
+    if (window == NULL)
+    {
+        cout << "Failed to open GLFW window" << endl;
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        cout << "Failed to initialize GLAD" << endl;
+        return -1;
+    }
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Create graphics buffer
+    Buffer buffer;
+    buffer.width  = buffer_width;
+    buffer.height = buffer_height;
+    buffer.data   = new uint32_t[buffer.width * buffer.height];
+
+    buffer_clear(&buffer, 0);
+
+    // Create texture
+    GLuint buffer_texture;
+    glGenTextures(1, &buffer_texture);
+    glBindTexture(GL_TEXTURE_2D, buffer_texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, buffer.width, buffer.height, 0, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8, buffer.data);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+
+    // Create vao for generating fullscreen triangle
+    GLuint fullscreen_triangle_vao;
+    glGenVertexArrays(1, &fullscreen_triangle_vao);
+
+
+    create_shaders();
+
+
+    //OpenGL setup
+    glDisable(GL_DEPTH_TEST);
+    glActiveTexture(GL_TEXTURE0);
+
+    glBindVertexArray(fullscreen_triangle_vao);
+
+    // Create Sprite
+    Sprite sprite;
+    sprite.width = 11;
+    sprite.height = 8;
+    sprite.data = new uint8_t[88]
+    {
+        0,0,0,0,0,0,0,0,0,0,0, // ...........
+        0,0,0,0,0,0,0,0,0,0,0, // ...........
+        0,0,1,1,1,1,1,1,1,0,0, // ..@@@@@@@..
+        0,1,1,0,1,1,1,0,1,1,0, // .@@.@@@.@@.
+        1,1,1,1,1,1,1,1,1,1,1, // @@@@@@@@@@@
+        1,0,1,1,1,1,1,1,1,0,1, // @.@@@@@@@.@
+        1,0,1,0,0,0,0,0,1,0,1, // @.@.....@.@
+        0,0,0,1,0,0,0,1,0,0,0  // ...@...@...
+    };
+
+    uint32_t clear_color = rgba_to_uint32(0, 128, 0, 255);
+
+    // Game loop
+    while (!glfwWindowShouldClose(window))
+    {
+        buffer_clear(&buffer, clear_color);
+
+        buffer_draw_sprite(&buffer, sprite, 112, 128, rgba_to_uint32(128, 0, 0, 255));
+
+        glTexSubImage2D(
+            GL_TEXTURE_2D, 0, 0, 0,
+            buffer.width, buffer.height,
+            GL_RGBA, GL_UNSIGNED_INT_8_8_8_8,
+            buffer.data
+        );
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    glDeleteVertexArrays(1, &fullscreen_triangle_vao);
+
+    delete[] sprite.data;
+    delete[] buffer.data;
+
+    return 0;
+}
+
+
+
